f3d89259a19ab96c31ee570af6ae100314663764
StudentA <cathychen.123456@gmail.com>
2019-10-14 00:13:38 +0800

    chap 05-9

--- hw_chap05_108820002/chap05_project09/chap05_project09.c
+++ hw_chap05_108820002/chap05_project09/chap05_project09.c
@@ -9,6 +9,26 @@
 
 #include<stdio.h>
 int main(){
-    
+    int month1, day1, year1,
+        month2, day2, year2;
+    printf("Enter first date(mm/dd/yy):");
+    scanf("%d/%d/%d",&month1,&day1,&year1);
+    printf("\nEnter second date(mm/dd/yy):");
+    scanf("%d/%d/%d",&month2,&day2,&year2);
+
+    for (int i = 0; i < 3; i++){
+        if (year1>year2){
+            printf("%d/%d/%d is earlier than %d/%d/%d",month1,day1,year1,month2,day2,year2);
+            break;
+        }
+        else if(year2>year1){
+            printf("%d/%d/%d is earlier than %d/%d/%d",month2,day2,year2,month1,day1,year1);
+            break;
+        }
+        
+        
+    }
+        
+
     return 0;
 }
