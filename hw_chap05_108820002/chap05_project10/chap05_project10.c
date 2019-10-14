a07670a72af95d92282f5a8f720fc1431be31b6c
StudentA <cathychen.123456@gmail.com>
2019-10-14 14:59:15 +0800

    chp 05-10

--- hw_chap05_108820002/chap05_project10/chap05_project10.c
+++ hw_chap05_108820002/chap05_project10/chap05_project10.c
@@ -6,8 +6,33 @@
 /* Purpose: chap05-10                                            */
 /* Change History: log the change history of the program         */
 /*****************************************************************/
-
 #include<stdio.h>
 int main(){
+    int a;
+    printf("Enter numerical grade : ");
+    scanf("%d",&a);                         //輸入
+    if ((a>100)||(a<0)){
+        printf("Error\n");                  //輸出錯誤
+    }
+    else{
+        switch (a/10){
+        case 10:
+        case 9:
+            printf("Letter grade: A\n");                         //輸出A
+            break;
+        case 8:
+            printf("Letter grade: B\n");                         //輸出B
+            break;
+        case 7:
+            printf("Letter grade: C\n");                         //輸出C
+            break; 
+        case 6:
+            printf("Letter grade: D\n");                         //輸出D
+            break;   
+        default:
+            printf("Letter grade: F\n");                         //輸出F
+            break;
+        }        
+    }
     return 0;
 }
