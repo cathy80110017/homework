68c3f3139c9bd6ecefd28ac0dfb6f6638381e5ac
StudentA <cathychen.123456@gmail.com>
2019-11-05 13:15:24 +0800

    update

---
+++ hw_chap09_108820002/chap09_project03/chap08_project07.c
@@ -0,0 +1,19 @@
+/*****************************************************************/
+/* Class: Computer Programming, Fall 2019                        */
+/* Author: 陳佳吟                                                 */
+/* ID: 108820002                                                 */
+/* Date: 2019.10.29                                             */
+/* Purpose: 07-10                                                */
+/* Change History: log the change history of the program         */
+/*****************************************************************/
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(){
+   
+    return 0;
+}
---
+++ hw_chap09_108820002/chap09_project06/chap08_project09.c
@@ -0,0 +1,15 @@
+/*****************************************************************/
+/* Class: Computer Programming, Fall 2019                        */
+/* Author: 陳佳吟                                                 */
+/* ID: 108820002                                                 */
+/* Date: 2019.10.29                                             */
+/* Purpose: chap07-11                                            */
+/* Change History: log the change history of the program         */
+/*****************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    
+    return 0;
+}
