23f8f1f32466fe5adf1f1c23683ab90789dee98b
StudentA <cathychen.123456@gmail.com>
2019-11-05 20:31:42 +0800

    chap08

--- hw_chap08_108820002/chap08_project07/chap08_project07.c
+++ hw_chap08_108820002/chap08_project07/chap08_project07.c
@@ -2,8 +2,8 @@
 /* Class: Computer Programming, Fall 2019                        */
 /* Author: 陳佳吟                                                 */
 /* ID: 108820002                                                 */
-/* Date: 2019.10.29                                             */
-/* Purpose: 07-10                                                */
+/* Date: 2019.11.05                                              */
+/* Purpose: 08-07                                                */
 /* Change History: log the change history of the program         */
 /*****************************************************************/
 
@@ -14,6 +14,30 @@
 #include<ctype.h>
 
 int main(){
-   
+    char ch, sentence[100] = {' '}, terminator = '.';
+    int i = 0, start, finish;
+
+    printf("Enter a sentence : ");
+    for (i = 1; i < 100; i++){
+        ch = getchar();                 //輸入
+        if (ch == '.'  || ch == '?' || ch == '!'){  //如果遇標點符號則跳出
+            terminator = ch;
+            break;
+        }
+        sentence[i] = ch;
+    }
+
+    printf("Reversal of sentence :");
+    finish = i;
+    for (start = finish - 1; start >= 0; start--){
+        if (sentence[start] == ' '){
+            for (int j = start; j < finish; j++){
+                putchar(sentence[j]);
+            }
+            finish = start;            
+        }
+    }
+    printf("%c\n", terminator);         //輸出標點佛號
+    
     return 0;
 }
