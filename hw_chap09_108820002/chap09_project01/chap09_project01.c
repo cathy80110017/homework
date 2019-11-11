993df98f3d71abaea324d3e5d3ae89498b9841b6
StudentA <cathychen.123456@gmail.com>
2019-11-11 15:16:15 +0800

    update

--- hw_chap09_108820002/chap09_project01/chap09_project01.c
+++ hw_chap09_108820002/chap09_project01/chap09_project01.c
@@ -13,31 +13,38 @@
 #include<string.h>
 #include<ctype.h>
 
-int selection_sort(int *arr, int a){
+void selection_sort(int arr[100], int a){
     for (int i = 0; i < a; i++){
-        int min = arr[i], num = i;
+        int min = arr[i], num = i;  //min預設為開始找的數
         for (int j = i; j < a; j++){
-            if (arr[j] < min){
-                num = j;
-                min = arr[j];
+            if (arr[j] < min){      //如果找到比min小的
+                num = j;            //存在第幾個找到的
+                min = arr[j];       //更改min
             }
         }
-        
+        arr[num] = arr[i];
+        arr[i] = min;
     }
-    
+
 }
 
 int main(){
-    int arr[100], i = 0;
+    int arr[100], i = 0, c, n = 0;
     printf("Enter list of intergers to be sorted: ");
-    while (scanf("%d",&arr[i]) != '\n'){
-        i++;
+    for (i = 0; i < 100; i++){
+        scanf("%d",&arr[i]);
+        n++;
+        if ((c = getchar()) == '\n'){
+            break;
+        }
+        ungetc(c, stdin);
     }
-    selection_sort(arr, i);
+    
+    selection_sort(arr, n);
 
     printf("Sorted list: ");
-    for (int n = 0; n < i ; n++){
-        printf("%d",arr[n]);
+    for (i = 0; i < n ; i++){
+        printf("%d ",arr[i]);
     }
     
     return 0;
